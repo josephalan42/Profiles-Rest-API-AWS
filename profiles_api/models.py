dd4e75a67c6a91efe238f6f67ea43a31ad80db34
Joseph Alan J <joesphalan17201972@gmail.com>
2021-06-17 10:33:32 +0530

    Added custom user profile model,manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,52 +1,58 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+
+
 from django.contrib.auth.models import BaseUserManager
 
 
 
 class UserProfileManager(BaseUserManager):
-        """Manager for user profiles"""
-        def create_user(self, email ,name ,password=None):
-            if not email:
-                raise ValueError('User must have an email address')
-            email =self.normalise_email(self)
-            user= self.model(email=email,name=name)
+    """Manager for user profiles"""
 
-            user.set_password(password)
-            user.save(using=self._db)
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('Users must have an email address')
 
-            return user
-        def create_superuser(self ,email ,name ,password):
-            """Create new superuser with given details"""
-            user= self.create_user(email,name,password)
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
 
-            user.is_superuser=True
-            user.is_staff=True
-            user.save(using=self._db)
+        user.set_password(password)
+        user.save(using=self._db)
 
-            return user
+        return user
 
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-class UserProfile(AbstractBaseUser,PermissionsMixin):
-    """Database Model for users in the system"""
-    email=models.EmailField(max_length=255, unique=True)
-    name=models.CharField(max_length=255)
-    is_active=models.BooleanField(default=True)
-    is_staff=models.BooleanField(default=False)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
+        return user
 
-    objects= UserProfileManager()
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-    USERNAME_FIELD='email'
-    REQUIRED_FIELDS=['name']
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrieve full name of user"""
+        """Retrieve full name for user"""
         return self.name
+
     def get_short_name(self):
         """Retrieve short name of user"""
         return self.name
-    def _str_(self):
-        """Return String Reprensentation of our user"""
+
+    def __str__(self):
+        """Return string representation of user"""
         return self.email
